477907ec80fbaac9014f61d170eae791e38504b8
wajiha <Wajiha.ahmed04@gmail.com>
2018-12-01 13:36:28 -0800

    checkpoint-six: completed

--- app.js
+++ app.js
@@ -1,4 +1,37 @@
 function onReady() {
+     const toDos = [];
+     const addToDoForm = document.getElementById('addToDoForm');
+     function createNewToDo() {
+  const newToDoText = document.getElementById('newToDoText');
+  if (!newToDoText.value) { return; }
+  
+  toDos.push({
+      title: newToDoText.value,
+      complete: false
+    });
+    newToDoText.value = '';
+    renderTheUI();
+  }
+  function renderTheUI() {
+    const toDoList = document.getElementById('toDoList');
+
+    toDoList.textContent = '';
+    toDos.forEach(function(toDo) {
+      const newLi = document.createElement('li');
+            const checkbox = document.createElement('input');
+            checkbox.type = "checkbox";
+              newLi.textContent = toDo.title;
+
+      toDoList.appendChild(newLi);
+      newLi.appendChild(checkbox);
+  });
+
+    }
+  addToDoForm.addEventListener('submit', event => {
+    event.preventDefault();
+    createNewToDo();
+  });
+
   const addToDoForm = document.getElementById('addToDoForm');
   const newToDoText = document.getElementById('newToDoText');
   const toDoList = document.getElementById('toDoList');
@@ -43,6 +76,7 @@ function onReady() {
       toDoList.removeChild(deleteButton);
     });
   });
+  renderTheUI();
 }
 
 
